a456d16cf3f9e4a217a431adeb9e522f80e8e964
GH-Rumi <helmirizqiramadhan10@gmail.com>
2024-09-05 11:12:39 +0700

    Add files via upload

---
+++ Biodata_11.java
@@ -0,0 +1,15 @@
+public class Biodata_11 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Helmi Rizqi Ramadhan");
+        System.out.println("NIM     : 244107020107");
+        System.out.println("Kelas   : TI-1H");
+        System.out.println("No Absen: 11");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : JL. Sultan Agung No 98 Kota Kediri");
+        System.out.println("No HP   : 0895343440640");
+        System.out.println("Hobi    : Olahraga, Gaming, Editing");
+    }
+}
