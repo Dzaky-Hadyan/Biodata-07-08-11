c045e42370815e2ac5a438f96beeb97f61045b09
Dzaky Hadyan Eliyanta <dzakyhadyaneliyanta06@gmail.com>
2024-09-05 10:59:16 +0700

    Biodata Dzaky

---
+++ Biodata_07.java
@@ -0,0 +1,15 @@
+public class Biodata_07 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Dzaky Hadyan Eliyanta");
+        System.out.println("NIM      : 244107020117");
+        System.out.println("Kelas    : 1-H");
+        System.out.println("No Absen : 07");
+        System.out.println("Prodi    : D-IV Teknik Informatika");
+        System.out.println("Alamat   : Jl. Anila IV 9F/22");
+        System.out.println("No HP    : 081252771100");
+        System.out.println("Hobi     : Bersepeda");
+    }
+}
